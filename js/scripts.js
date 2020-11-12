09f45180d26d9f4fd48b9fd47a981269c006b0bc
amandaLuana <amandaluana008@gmail.com>
2020-11-11 21:52:47 -0300

    alterações no input 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 document.querySelector('form input').oninvalid = function() {
-    // remove mensagens de erro padrão
+    // remove mensagem de erro padrão
     this.setCustomValidity("");
 
     // faz a validação novamente
     if (!this.validity.valid) {
-        this.setCustomValidity("Please provide a valid email");
+        //this.setCustomValidity("Please provide a valid email");
+        var req = document.querySelector('form p');
+        req.innerHTML = 'Please provide a valid email';
+        
+        var display = document.querySelector('form img#icon-error');
+        display.style.display = 'block';
     }
 }
